026fca680d678eeb15c8db5ecfd87f765c96977f
二叉树树 <acofork@qq.com>
2026-01-17 18:57:28 +0800

    Add short link: pO3y7no

--- js/rules_intermediate.js
+++ js/rules_intermediate.js
@@ -2662,5 +2662,9 @@ window.RULES_INTERMEDIATE = {
     "/kH5CnzP": {
         "url": "https://developer.mozilla.org/zh-CN/docs/Web/JavaScript",
         "expired_at": "2026-01-21T17:00:00.000Z"
+    },
+    "/pO3y7no": {
+        "url": "https://developer.mozilla.org/zh-CN/docs/Web/JavaScript",
+        "expired_at": "2026-01-21T17:00:00.000Z"
     }
 };
