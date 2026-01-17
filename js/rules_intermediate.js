53a2870345352543571ffc74f8675a4b266986af
二叉树树 <acofork@qq.com>
2026-01-17 18:56:05 +0800

    Add short link: VG9MeCH

--- js/rules_intermediate.js
+++ js/rules_intermediate.js
@@ -2498,5 +2498,9 @@ window.RULES_INTERMEDIATE = {
     "/sd0g4Mc": {
         "url": "https://developer.mozilla.org/zh-CN/docs/Web/JavaScript",
         "expired_at": "2026-01-21T17:00:00.000Z"
+    },
+    "/VG9MeCH": {
+        "url": "https://developer.mozilla.org/zh-CN/docs/Web/JavaScript",
+        "expired_at": "2026-01-21T17:00:00.000Z"
     }
 };
