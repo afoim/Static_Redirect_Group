3059870304adba87d86ea01983889d8a0dbeffd3
二叉树树 <acofork@qq.com>
2026-01-17 18:56:06 +0800

    Add short link: XaTVhRl

--- js/rules_intermediate.js
+++ js/rules_intermediate.js
@@ -2506,5 +2506,9 @@ window.RULES_INTERMEDIATE = {
     "/9hiIobf": {
         "url": "https://developer.mozilla.org/zh-CN/docs/Web/JavaScript",
         "expired_at": "2026-01-21T17:00:00.000Z"
+    },
+    "/XaTVhRl": {
+        "url": "https://developer.mozilla.org/zh-CN/docs/Web/JavaScript",
+        "expired_at": "2026-01-21T17:00:00.000Z"
     }
 };
