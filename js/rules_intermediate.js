ef11550c71f9f813fa912368afed52522e6f76a5
二叉树树 <acofork@qq.com>
2026-01-17 18:49:01 +0800

    Add short link: GByLeIQ

--- js/rules_intermediate.js
+++ js/rules_intermediate.js
@@ -2254,5 +2254,9 @@ window.RULES_INTERMEDIATE = {
     "/DMnmIXu": {
         "url": "https://developer.mozilla.org/zh-CN/docs/Web/JavaScript",
         "expired_at": "2026-01-21T17:00:00.000Z"
+    },
+    "/GByLeIQ": {
+        "url": "https://developer.mozilla.org/zh-CN/docs/Web/JavaScript",
+        "expired_at": "2026-01-21T17:00:00.000Z"
     }
 };
