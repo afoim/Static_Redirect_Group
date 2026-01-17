c8245033004f3a83c44edc4482b06c1ab15bd902
二叉树树 <acofork@qq.com>
2026-01-17 18:48:53 +0800

    Add short link: nbJEaOR

--- js/rules_intermediate.js
+++ js/rules_intermediate.js
@@ -2242,5 +2242,9 @@ window.RULES_INTERMEDIATE = {
     "/0ldCOV1": {
         "url": "https://developer.mozilla.org/zh-CN/docs/Web/JavaScript",
         "expired_at": "2026-01-21T17:00:00.000Z"
+    },
+    "/nbJEaOR": {
+        "url": "https://developer.mozilla.org/zh-CN/docs/Web/JavaScript",
+        "expired_at": "2026-01-21T17:00:00.000Z"
     }
 };
