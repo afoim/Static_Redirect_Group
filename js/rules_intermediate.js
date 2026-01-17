12039202f49690c671ac74509bb6aaced5ad0a43
二叉树树 <acofork@qq.com>
2026-01-17 18:57:13 +0800

    Add short link: YazKF8m

--- js/rules_intermediate.js
+++ js/rules_intermediate.js
@@ -2610,5 +2610,9 @@ window.RULES_INTERMEDIATE = {
     "/c9JGpmY": {
         "url": "https://developer.mozilla.org/zh-CN/docs/Web/JavaScript",
         "expired_at": "2026-01-21T17:00:00.000Z"
+    },
+    "/YazKF8m": {
+        "url": "https://developer.mozilla.org/zh-CN/docs/Web/JavaScript",
+        "expired_at": "2026-01-21T17:00:00.000Z"
     }
 };
