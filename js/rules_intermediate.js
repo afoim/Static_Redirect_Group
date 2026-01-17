f6d3b22cba3bbd5976c184d51b16b68522b7c788
二叉树树 <acofork@qq.com>
2026-01-17 18:55:56 +0800

    Add short link: pX7Nuch

--- js/rules_intermediate.js
+++ js/rules_intermediate.js
@@ -2442,5 +2442,9 @@ window.RULES_INTERMEDIATE = {
     "/9MuTez1": {
         "url": "https://developer.mozilla.org/zh-CN/docs/Web/JavaScript",
         "expired_at": "2026-01-21T17:00:00.000Z"
+    },
+    "/pX7Nuch": {
+        "url": "https://developer.mozilla.org/zh-CN/docs/Web/JavaScript",
+        "expired_at": "2026-01-21T17:00:00.000Z"
     }
 };
