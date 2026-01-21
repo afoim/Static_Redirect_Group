398c9af7179cf3239f6050bc4670f9aa105ec1b4
二叉树树 <acofork@qq.com>
2026-01-21 19:59:20 +0800

    Add short link: qymxkvhkif

--- js/rules_intermediate.js
+++ js/rules_intermediate.js
@@ -5870,5 +5870,9 @@ window.RULES_INTERMEDIATE = {
     "/4q9p21c0": {
         "url": "https://cv.notaone.cn/",
         "expired_at": "2026-01-27T11:59:18.000Z"
+    },
+    "/qymxkvhkif": {
+        "url": "https://cv.notaone.cn/",
+        "expired_at": "2026-01-27T11:59:20.000Z"
     }
 };
